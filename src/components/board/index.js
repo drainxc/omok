28b2f87254fc0794017d6e79f392d1d4eae0b0b4
ldh7228 <ldh7228@gmail.com>
2021-11-09 22:50:56 +0900

    [FIX] compare

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -4,16 +4,21 @@ import { board } from "../../lib/export/index";
 
 export default function Board() {
   function compare(a, b, c, d) {
-    if (board[a][b] === board[c][d] && board[a][b] === 1) {
+    if (board[a][b] === board[c][d] && board[a][b] !== 0) {
       count += 1;
     }
     if (count === 4) {
-      console.log("asdf");
+      if (board[a][b] === 1) {
+          console.log("흑 승리");
+      }
+      else {
+          console.log("백 승리");
+      }
     }
   }
   console.log(board);
   let count = 0;
-  for (let x = 0; x < 15; x++) {
+  for (let x = 0; x <= 10; x++) {
     for (let y = 0; y <= 10; y++) {
       for (let n = 1; n < 5; n++) {
         compare(y, x, y, x + n);
