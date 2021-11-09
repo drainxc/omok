43616d9416ac266e0be0ca9720f9e6f90bd3fcb6
ldh7228 <ldh7228@gmail.com>
2021-11-09 19:30:06 +0900

    [ADD] board

---
+++ src/components/board/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  width: 450px;
+  height: 450px;
+  border-collapse: collapse;
+  th {
+    border: 1px solid black
+  }
+`;
