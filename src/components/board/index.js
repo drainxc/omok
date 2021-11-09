1a0a6e27ba6d14c24974425606e39dd77491e18d
ldh7228 <ldh7228@gmail.com>
2021-11-09 22:41:25 +0900

    [REFACTOR] compare

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -3,44 +3,32 @@ import * as S from "./styles";
 import { board } from "../../lib/export/index";
 
 export default function Board() {
+  function compare(a, b, c, d) {
+    if (board[a][b] === board[c][d] && board[a][b] === 1) {
+      count += 1;
+    }
+    if (count === 4) {
+      console.log("asdf");
+    }
+  }
   console.log(board);
   let count = 0;
   for (let x = 0; x < 15; x++) {
     for (let y = 0; y <= 10; y++) {
       for (let n = 1; n < 5; n++) {
-        if (board[y][x] === board[y][x + n] && board[y][x] === 1) {
-          count += 1;
-        }
-        if (count === 4) {
-          console.log("asdf");
-        }
+        compare(y, x, y, x + n);
       }
       count = 0;
       for (let n = 1; n < 5; n++) {
-        if (board[y][x] === board[y + n][x] && board[y][x] === 1) {
-          count += 1;
-        }
-        if (count === 4) {
-          console.log("asdf");
-        }
+        compare(y, x, y + n, x);
       }
       count = 0;
       for (let n = 1; n < 5; n++) {
-        if (board[y][x] === board[y + n][x + n] && board[y][x] === 1) {
-          count += 1;
-        }
-        if (count === 4) {
-          console.log("asdf");
-        }
+        compare(y, x, y + n, x + n);
       }
       count = 0;
       for (let n = 1; n < 5; n++) {
-        if (board[y][x] === board[y + n][x - n] && board[y][x] === 1) {
-          count += 1;
-        }
-        if (count === 4) {
-          console.log("asdf");
-        }
+        compare(y, x, y + n, x - n);
       }
       count = 0;
     }
