a651239dd880110c5f3b969881a584d94895b2a1
ldh7228 <ldh7228@gmail.com>
2021-11-10 13:27:49 +0900

    [ADD] put

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -7,26 +7,29 @@ export default function Board() {
   const [board, SetBoard] = useState(data);
 
   const [put, setPut] = useState(false);
-  const change = useCallback(()=> {
-    console.log(1);
-  setPut(!put);
-  SetBoard(board, (board[1][4] = 1));
-},[board, put])
+  const change = useCallback((i, j) => {
+    setPut(!put);
+    SetBoard(board, (board[i][j] = 1));
+  }, [board, put]);
 
-  const boardMemo = useMemo(()=>{
-      console.log(board);
-    return(<>{ 
-      board.map((boardN,i) => (
-    <tr>
-      {boardN.map((index,j) => (
-        <th>
-            <button id ={`${i} ${j}`} onClick={change}>{index}</button>
-        </th>
-      ))}
-    </tr>
-  ))}</>)},[board, change]);
-
-
+  const boardMemo = useMemo(() => {
+    console.log(board);
+    return (
+      <>
+        {board.map((boardN, i) => (
+          <tr>
+            {boardN.map((index, j) => (
+              <th>
+                <button id={`${i} ${j}`} onClick={() => change(i, j)}>
+                  {index}
+                </button>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </>
+    );
+  }, [board, change]);
 
   return (
     <>
