7713d123422cc256e6da1aa535bd0f1b685e9b2c
ldh7228 <ldh7228@gmail.com>
2021-11-10 19:27:14 +0900

    [ADD] heightCompare
    세로로 비교 추가

--- src/components/compare/index.js
+++ src/components/compare/index.js
@@ -1,18 +1,25 @@
 export function Compare(y, x, board) {
   let point = 0;
-  widthCompare(1);
-  widthCompare(-1);
+  compare(1, 0);
+  compare(-1, 0);
   if (point >= 4) {
     alert("흑 승리");
   }
   else {
       point = 0;
   }
-  
+  compare(0, 1);
+  compare(0, -1);
+  if (point >= 4) {
+    alert("흑 승리");
+  }
+  else {
+      point = 0;
+  }
 
-  function widthCompare(width) {
+  function compare(width, height) {
     for (let i = 1; i < 5; i++) {
-      if (board[y][x] === board[y][x + width * i] && board[y][x] !== 0) {
+      if (board[y][x] === board[y + (height * i)][x + (width * i)] && board[y][x] !== 0) {
         point += 1;
         console.log(point);
       } else {
