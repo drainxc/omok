1052dadca6125d34e56d3d6ceca866a3f9716275
ldh7228 <ldh7228@gmail.com>
2021-11-10 19:56:28 +0900

    [ADD] diagonal compare

--- src/components/compare/index.js
+++ src/components/compare/index.js
@@ -1,29 +1,34 @@
 export function Compare(y, x, board) {
   let point = 0;
-  compare(1, 0);
-  compare(-1, 0);
-  if (point >= 4) {
-    alert("흑 승리");
-  }
-  else {
-      point = 0;
-  }
-  compare(0, 1);
-  compare(0, -1);
-  if (point >= 4) {
-    alert("흑 승리");
-  }
-  else {
-      point = 0;
-  }
+  mainCompare(1, 0);
+  mainCompare(-1, 0);
+  point = 0;
+  mainCompare(0, 1);
+  mainCompare(0, -1);
+  point = 0;
+  mainCompare(1, 1);
+  mainCompare(-1, -1);
+  point = 0;
+  mainCompare(1, -1);
+  mainCompare(-1, 1);
+  point = 0;
 
-  function compare(width, height) {
+  function mainCompare(width, height) {
     for (let i = 1; i < 5; i++) {
-      if (board[y][x] === board[y + (height * i)][x + (width * i)] && board[y][x] !== 0) {
+      if (
+        board[y][x] === board[y + height * i][x + width * i] &&
+        board[y][x] !== 0
+      ) {
         point += 1;
         console.log(point);
       } else {
         i = 5;
+      }
+      if (point === 4 && board[y][x] == 1) {
+        alert("흑 승리!");
+      }
+      else if (point === 4 && board[y][x] == 2) {
+          alert("백 승리!");
       }
     }
   }
