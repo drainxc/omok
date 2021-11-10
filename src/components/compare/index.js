23cc8445062349dd043b5b49c4c0feadecf02c69
ldh7228 <ldh7228@gmail.com>
2021-11-10 19:26:27 +0900

    [FIX] compare

--- src/components/compare/index.js
+++ src/components/compare/index.js
@@ -1,40 +1,23 @@
-import React from "react";
-
-let count = 0;
-
-function MainCompare(a, b, c, d, board) {
-  if (board[a][b] === board[c][d] && board[a][b] !== 0) {
-    count += 1;
-    console.log(count);
+export function Compare(y, x, board) {
+  let point = 0;
+  widthCompare(1);
+  widthCompare(-1);
+  if (point >= 4) {
+    alert("흑 승리");
   }
-  if (count === 4) {
-    if (board[a][b] === 1) {
-      console.log("흑 승리");
-    } else {
-      console.log("백 승리");
-    }
+  else {
+      point = 0;
   }
-}   
+  
 
-export function Compare(board) {
-  for (let x = 0; x <= 14; x++) {
-    for (let y = 0; y <= 10; y++) {
-      for (let n = 1; n < 5; n++) {
-        MainCompare(y, x, y, x + n, board);
+  function widthCompare(width) {
+    for (let i = 1; i < 5; i++) {
+      if (board[y][x] === board[y][x + width * i] && board[y][x] !== 0) {
+        point += 1;
+        console.log(point);
+      } else {
+        i = 5;
       }
-      count = 0;
-      for (let n = 1; n < 5; n++) {
-        MainCompare(y, x, y + n, x, board);
-      }
-      count = 0;
-      for (let n = 1; n < 5; n++) {
-        MainCompare(y, x, y + n, x + n, board);
-      }
-      count = 0;
-      for (let n = 1; n < 5; n++) {
-        MainCompare(y, x, y + n, x - n, board);
-      }
-      count = 0;
     }
   }
 }
