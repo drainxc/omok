7ad9cc57115d9c3d15550a97e7b1e23547ac02f5
ldh7228 <ldh7228@gmail.com>
2021-11-11 14:53:46 +0900

    [FIX] put stone
    흑돌 위치에 돌 놓기 막기

--- src/components/AI/index.js
+++ src/components/AI/index.js
@@ -8,11 +8,15 @@ export function Ai(coordinate, setBoard) {
     Compare(1, 1, -1, -1, coordinate) === 0 &&
     Compare(1, -1, -1, 1, coordinate) === 0
   ) {
-    y = coordinate.y + getRandomIntInclusive(-1, 1);
-    x = coordinate.x + getRandomIntInclusive(-1, 1);
-    setBoard(coordinate.board, (coordinate.board[y][x] = 2));
-    return (document.getElementById(`${y} ${x}`).style =
-      "opacity: 1; background-color: white;");
+    while (1) {
+      y = coordinate.y + getRandomIntInclusive(-1, 1);
+      x = coordinate.x + getRandomIntInclusive(-1, 1);
+      if (coordinate.board[y][x] === 0) {
+        setBoard(coordinate.board, (coordinate.board[y][x] = 2));
+        return (document.getElementById(`${y} ${x}`).style =
+          "opacity: 1; background-color: white;");
+      }
+    }
   }
 }
 
