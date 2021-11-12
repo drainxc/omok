2ee8471d710082703a7a2cf212e7deda9bc15616
ldh7228 <ldh7228@gmail.com>
2021-11-12 11:56:23 +0900

    [ADD] AI
    Add AI Attack

--- src/components/common/AI/index.js
+++ src/components/common/AI/index.js
@@ -1,5 +1,7 @@
 import { Compare, EverythingCompare, MainCompare } from "../compare";
 
+let beforeX, beforeY;
+
 export function Ai(coordinate, setBoard) {
   let y, x; // 좌표 선언
   const drawStyle = "opacity: 1; background-color: white;"; // 색 및 투명도
@@ -17,39 +19,60 @@ export function Ai(coordinate, setBoard) {
         // 빈 배열일 때
         setBoard(coordinate.board, (coordinate.board[y][x] = 2)); // 배열 넣기
         EverythingCompare(coordinate.board, y, x);
+        beforeX = x;
+        beforeY = y;
         return (document.getElementById(`${y} ${x}`).style = drawStyle); // 돌 그리기
       }
     }
   } else {
     for (let i = 3; i > 0; i--) {
-      if (
+      console.log(Compare(1, 0, -1, 0, coordinate.board, beforeY, beforeX));
+      if (Compare(1, 0, -1, 0, coordinate.board, beforeY, beforeX) >= i) {
+        Put(1, 0, -1, 0, beforeY, beforeX);
+        return (document.getElementById(`${y} ${x}`).style = drawStyle);
+      } else if (
+        Compare(0, 1, 0, -1, coordinate.board, beforeY, beforeX) >= i
+      ) {
+        Put(0, 1, 0, -1, beforeY, beforeX);
+        return (document.getElementById(`${y} ${x}`).style = drawStyle);
+      } else if (
+        Compare(1, 1, -1, -1, coordinate.board, beforeY, beforeX) >= i
+      ) {
+        Put(1, 1, -1, -1, beforeY, beforeX);
+        return (document.getElementById(`${y} ${x}`).style = drawStyle);
+      } else if (
+        Compare(1, -1, -1, 1, coordinate.board, beforeY, beforeX) >= i
+      ) {
+        Put(1, -1, -1, 1, beforeY, beforeX);
+        return (document.getElementById(`${y} ${x}`).style = drawStyle);
+      } else if (
         Compare(1, 0, -1, 0, coordinate.board, coordinate.y, coordinate.x) >= i
       ) {
         // 가로로 공격을 받았을 때
-        defence(1, 0, -1, 0);
+        Put(1, 0, -1, 0, coordinate.y, coordinate.x);
         return (document.getElementById(`${y} ${x}`).style = drawStyle);
       } else if (
         Compare(0, 1, 0, -1, coordinate.board, coordinate.y, coordinate.x) >= i
       ) {
         // 세로로 공격을 받았을 때
-        defence(0, 1, 0, -1);
+        Put(0, 1, 0, -1, coordinate.y, coordinate.x);
         return (document.getElementById(`${y} ${x}`).style = drawStyle);
       } else if (
         Compare(1, 1, -1, -1, coordinate.board, coordinate.y, coordinate.x) >= i
       ) {
         // 대각선으로 공격을 받았을 때
-        defence(1, 1, -1, -1);
+        Put(1, 1, -1, -1, coordinate.y, coordinate.x);
         return (document.getElementById(`${y} ${x}`).style = drawStyle);
       } else if (
         Compare(1, -1, -1, 1, coordinate.board, coordinate.y, coordinate.x) >= i
       ) {
         // 대각선(반대)으로 공격을 받았을 떄
-        defence(1, -1, -1, 1);
+        Put(1, -1, -1, 1, coordinate.y, coordinate.x);
         return (document.getElementById(`${y} ${x}`).style = drawStyle);
       }
     }
 
-    function defence(x1, y1, x2, y2) {
+    function Put(x1, y1, x2, y2, Y, X) {
       const randomNumber = getRandomIntInclusive(0, 1);
       let tmp;
       if (randomNumber) {
@@ -61,63 +84,67 @@ export function Ai(coordinate, setBoard) {
         y2 = tmp;
       } // 랜덤으로 값 변경
       y = // y좌표
-        coordinate.y +
+        Y +
         y1 *
           (MainCompare(
             x1,
             y1,
             coordinate.board,
-            coordinate.y,
-            coordinate.x,
+            Y,
+            X,
             0
           ) +
             1);
       x = // x좌표
-        coordinate.x +
+        X +
         x1 *
           (MainCompare(
             x1,
             y1,
             coordinate.board,
-            coordinate.y,
-            coordinate.x,
+            Y,
+            X,
             0
           ) +
             1); // 공격을 받았을 때 수비
       if (coordinate.board[y][x] === 0) {
-        // 좌표가 빈 배열이라면 수비하고 리턴\
+        // 좌표가 빈 배열이라면 수비하고 리턴
         setBoard(coordinate.board, (coordinate.board[y][x] = 2));
         EverythingCompare(coordinate.board, y, x);
+        beforeX = x;
+        beforeY = y;
         // eslint-disable-next-line no-sequences
         return y, x;
       } else {
         // 좌표가 빈 배열이 아니라면 반대쪽을 수비하고 리턴
         y = // x좌표 반대쪽
-          coordinate.y +
+          Y +
           y2 *
             (MainCompare(
               x2,
               y2,
               coordinate.board,
-              coordinate.y,
-              coordinate.x,
+              Y,
+              X,
               0
             ) +
               1);
         x = // y좌표 반대쪽
-          coordinate.x +
+          X +
           x2 *
             (MainCompare(
               x2,
               y2,
               coordinate.board,
-              coordinate.y,
-              coordinate.x,
+              Y,
+              X,
               0
             ) +
               1);
         setBoard(coordinate.board, (coordinate.board[y][x] = 2));
         EverythingCompare(coordinate.board, y, x);
+        beforeX = x;
+        beforeY = y;
         // eslint-disable-next-line no-sequences
         return y, x;
       }
